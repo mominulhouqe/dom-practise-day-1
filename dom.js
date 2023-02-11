91e2f84b3a401146cb933ab0388c618bc64b0ae2
Mominul Hoque <88490184+Momin69@users.noreply.github.com>
2023-02-11 12:06:30 +0600

    first commit

---
+++ dom.js
@@ -0,0 +1,36 @@
+// const select = document.querySelectorAll('section');
+
+// for(const section of select ){
+//     section.style.border = '5px solid red';
+//     section.style.background = 'peru';
+//     section.style.color = ' white';
+//     section.style.marginBottom = ' 5px solid'
+// }
+// console.log(select);
+/* 
+const Changes = document.getElementById('helloJs');
+
+Changes.style.background = 'green';
+Changes.style.margin = ' 0 auto';
+Changes.style.width = ' 300px'
+Changes.style.padding = '20px';
+Changes.style.textAlign = 'center'; */
+
+/* 
+const addSomething = document.getElementById('fuck');
+
+const li = document.createElement('h4');
+li.innerText = ' I love your coock';
+
+addSomething.appendChild(li);
+
+*/
+
+/* 
+ Task --> To add appenChild 
+
+const addSomethingToChild = document.getElementById('secondPart');
+const btn = document.createElement('button');
+ btn.innerText = 'Skip your life';
+ addSomethingToChild.appendChild(btn);
+ */
